17cb75916cb89e298f0ada51fb1a200b2e931aff
Keshav Mishra <keshavmishra4922@gmail.com>
2025-07-12 13:52:58 +0530

    Initial commit - Clean history

---
+++ routes/videoRoutes.js
@@ -0,0 +1,35 @@
+import express from "express";
+
+import {
+  deleteVideo,
+  dislikeVideo,
+  getAllVideos,
+  getVideoById,
+  getVideosByLoggedInUser,
+  incraseViews,
+  likeVideo,
+  searchVideos,
+  updateVideo,
+  uploadVideo,
+} from "../controllers/VideoController.js";
+import { protect } from "../middleware/authMiddleware.js";
+import { uploadBoth } from "../middleware/multer.js";
+import commentRoutes from "./commetRoutes.js"
+
+const router = express.Router();
+
+
+router.use("/:videoId/comments", commentRoutes);
+
+router.get("/search", searchVideos);
+router.get("/", getAllVideos);
+router.get("/:id", getVideoById);
+router.get("/user", protect, getVideosByLoggedInUser);
+router.post("/upload", protect, uploadBoth, uploadVideo);
+router.put("/:id", protect, uploadBoth, updateVideo);
+router.delete("/:id", protect, deleteVideo);
+router.post("/:id/like", protect, likeVideo);
+router.post("/:id/dislike", protect, dislikeVideo);
+router.patch("/:id/views", incraseViews);
+
+export default router;
---
+++ server.js
@@ -0,0 +1,41 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import cors from "cors";
+
+// Routes
+import authRoutes from "./routes/authRoutes.js";
+import channelRoutes from "./routes/channelRoutes.js";
+import videoRoutes from "./routes/videoRoutes.js";
+
+// Error middleware
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// API Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/channels", channelRoutes);
+app.use("/api/videos", videoRoutes);
+
+// Error handling middlewares
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
