87e3b3e25e8a5f6c1147f3c3564a72e50952375b
Keshav Mishra <keshavmishra4922@gmail.com>
2025-07-13 14:12:41 +0530

    deploy 2

--- server.js
+++ server.js
@@ -16,14 +16,15 @@ connectDB();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
